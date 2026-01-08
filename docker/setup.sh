6296bcc60ad290fe375841f58105202a0fb4416a
Walter Boring <waboring@hemna.com>
2026-01-08 10:02:23 -0500

    Update setup.sh to allow options via env
    This patch allows you to set env vars in
    docker-compose.yml to configure how
    the exporter starts and gets it's stats

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,13 +1,34 @@
 #!/usr/bin/env bash
 set -x
 
-# The default command
-# Override the command in docker-compose.yml to change
-# what command you want to run in the container
-
+# Default values
 APRSD_URL=${APRSD_URL:-http://localhost:8080}
 UPDATE_INTERVAL=${UPDATE_INTERVAL:-60}
+HOST=${HOST:-0.0.0.0}
+PORT=${PORT:-8080}
+API_PORT=${API_PORT:-8081}
 
 source /app/.venv/bin/activate
 
-aprsd_exporter --aprsd-url ${APRSD_URL} --update-interval ${UPDATE_INTERVAL} --api --api-port 8081
+# Build the command
+CMD="aprsd_exporter --host ${HOST} --port ${PORT} --update-interval ${UPDATE_INTERVAL}"
+
+if [ -n "${STATS_FILE}" ]; then
+    CMD="${CMD} --stats-file ${STATS_FILE}"
+elif [ -n "${API}" ] && [ "${API}" = "true" ]; then
+    CMD="${CMD} --api --api-port ${API_PORT}"
+else
+    CMD="${CMD} --aprsd-url ${APRSD_URL}"
+fi
+
+# Add optional flags
+if [ -n "${DEBUG}" ] && [ "${DEBUG}" = "true" ]; then
+    CMD="${CMD} --debug"
+fi
+
+if [ -n "${QUIET}" ] && [ "${QUIET}" = "true" ]; then
+    CMD="${CMD} --quiet"
+fi
+
+# Execute the command
+exec ${CMD}
