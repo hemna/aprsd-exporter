738cd2940154da520e0892ef6a62cdbebcc8bb86
Walter Boring <waboring@hemna.com>
2026-01-13 09:44:08 -0500

    add tracemalloc

--- aprsd_exporter/__main__.py
+++ aprsd_exporter/__main__.py
@@ -3,11 +3,40 @@
 import asyncio
 import inspect
 import logging
+import linecache
+import tracemalloc
 
 import click
 from loguru import logger
 
 from .exporter import APRSDExporter
+
+
+def display_top_stats(snapshot, key_type="lineno", limit=5):
+    """Show the top offenders in memeory consumption."""
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
 
 
 class InterceptHandler(logging.Handler):
@@ -102,6 +131,7 @@ def main(
     aprsd_url, host, port, update_interval, debug, quiet, api, api_port, stats_file, max_seen_callsigns
 ):
     """Run prometheus exporter"""
+    tracemalloc.start()
     logging.getLogger("asyncio").setLevel(logging.ERROR)
     logging.getLogger("aiohttp").setLevel(logging.DEBUG)
     logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
@@ -146,6 +176,9 @@ def main(
             loop.run_until_complete(exp.stop())
     loop.stop()
     loop.close()
+
+    mem_snap = tracemalloc.take_snapshot()
+    display_top_stats(mem_snap)
 
 
 if __name__ == "__main__":
